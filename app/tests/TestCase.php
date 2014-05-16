b6b3b16b2372269494ed6bb1f3ca419c78f7cbb9
fanshine <fanshine124@gmail.com>
2014-05-16 17:35:01 +0800

    phpunit

--- app/tests/TestCase.php
+++ app/tests/TestCase.php
@@ -2,6 +2,15 @@
 
 class TestCase extends Illuminate\Foundation\Testing\TestCase {
 
+    /**
+     * Default preparation for each test
+     */
+    public function setUp()
+    {
+        parent::setUp();
+        $this->prepareForTests();
+    }
+
 	/**
 	 * Creates the application.
 	 *
@@ -15,5 +24,12 @@ class TestCase extends Illuminate\Foundation\Testing\TestCase {
 
 		return require __DIR__.'/../../bootstrap/start.php';
 	}
+    /**
+     * Migrate the database
+     */
+    private function prepareForTests()
+    {
+        Artisan::call('migrate');
+    }
 
 }
--- app/tests/UserTest.php
+++ app/tests/UserTest.php
@@ -1,7 +1,18 @@
 <?php
 class UserTest extends TestCase {
-    public function testThatTrueIsTrue ()
+    public function testUsernameIsRequired ()
     {
-        $this->assertTrue(true);
+        $user = new User;
+        $user->email = "phil@ipbrown.com";
+        $user->password = "password";
+        $user->password_confirmation = "password";
+
+        $this->assertFalse($user->save());
+
+        $error = $user->errors()->all();
+
+        $this->assertCount(1, $errors);
+
+        $this->assertEquals($errors[0], "The username field is reuqired.");
     }
 }
